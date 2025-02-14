86b47bc0a0993b654d8479e83c7e1233763fb5fb
torpedotucu <rodrigoorovio2@gmail.com>
2025-02-14 12:32:15 +0100

    ss

--- MvcNetCoreEFMultiplesBBDD/Data/HospitalContext.cs
+++ MvcNetCoreEFMultiplesBBDD/Data/HospitalContext.cs
@@ -1,6 +1,13 @@
-﻿namespace MvcNetCoreEFMultiplesBBDD.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCoreEFMultiplesBBDD.Models;
+
+namespace MvcNetCoreEFMultiplesBBDD.Data
 {
-    public class HospitalContext
+    public class HospitalContext:DbContext
     {
+        public HospitalContext(DbContextOptions<HospitalContext> options) 
+        :base(options){ }
+
+        public DbSet<EmpleadoView> EmpleadosViews { get; set; }
     }
 }
---
+++ MvcNetCoreEFMultiplesBBDD/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCoreEFMultiplesBBDD.Data;
+using MvcNetCoreEFMultiplesBBDD.Models;
+
+namespace MvcNetCoreEFMultiplesBBDD.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context=context;
+        }
+        
+        public async Task<List<EmpleadoView>> GetEmpleadosAsync()
+        {
+            var consulta = from datos in this.context.EmpleadosViews
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<EmpleadoView>FindEmpleadoAsync(int idEmpleado)
+        {
+            var consulta = from datos in this.context.EmpleadosViews
+                           where datos.IdEmpleado==idEmpleado
+                           select datos;
+            return await consulta.FirstOrDefaultAsync();
+        }
+
+    }
+}
